ca1c82e833c91bca1ef588f4192a6c158436e902
RobertCioroba <ciorobarobert18@gmail.com>
2020-06-05 21:34:00 +0300

    auth

---
+++ Assets/CreateScrollList.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class retete
+{
+    public string pasi;
+    public Sprite wow;
+    public Button.ButtonClickedEvent spreBucatar;
+}
+
+
+public class CreateScrollList : MonoBehaviour
+{
+    public GameObject sampleButton;
+    public Transform contentPanel;
+    public List<retete> listaRetete;
+
+    void Start()
+    {
+        PopulateList ();
+    }
+
+    void PopulateList()
+    {
+        foreach (var item in listaRetete)
+            {
+            GameObject newButton = Instantiate(sampleButton) as GameObject;
+            SampleButtonScript buttonScript = newButton.GetComponent<SampleButtonScript > ();
+            buttonScript.description.text = item.pasi;
+            buttonScript.wow.sprite = item.wow;
+            buttonScript.button.onClick = item.spreBucatar;
+
+            newButton.transform.SetParent(contentPanel);
+            }
+    }
+
+    public void MergiLaBucatar()
+    {
+        SceneManager.LoadScene("mainn");
+        Debug.Log("Esti pe drumul cel bun!");
+    }
+}
---
+++ Assets/Scripts/DataBridge.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using Firebase.Auth;
+
+
+public class DataBridge : MonoBehaviour {
+
+    public Text usernameInput, passwordInput;
+
+    private Player data;
+
+    private string DATA_URL = "https://pocket-chef-firebase.firebaseio.com/";
+
+    private DatabaseReference databaseReference;
+
+    void Start() {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl(DATA_URL);
+
+        databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    public void SaveData() { 
+
+        if(usernameInput.text.Equals("") && passwordInput.text.Equals("")) {
+            print("NO DATA");
+            return;
+        } 
+
+        data = new Player(usernameInput.text, passwordInput.text);
+
+        string jsonData = JsonUtility.ToJson(data);
+
+        databaseReference.Child("Users" + Random.Range(0, 1000000)).
+            SetRawJsonValueAsync(jsonData);
+            
+    }
+
+    public void LoadData() {
+
+        FirebaseDatabase.DefaultInstance.GetReferenceFromUrl(DATA_URL).GetValueAsync()
+                    .ContinueWith((task => {
+
+                        if (task.IsCanceled) {
+
+                        }
+
+                        if (task.IsFaulted) { 
+                            
+                        }
+
+                        if (task.IsCompleted) {
+
+                            DataSnapshot snapshot = task.Result;
+
+                            string playerData = snapshot.GetRawJsonValue();
+
+                            //Player m = JsonUtility.FromJson<Player>(playerData);
+
+                            foreach(var child in snapshot.Children) {
+
+                                string t = child.GetRawJsonValue();
+
+                                Player extractedData = JsonUtility.FromJson<Player>(t);
+
+                                print("The Player is: " + extractedData.Username);
+                                print("The Player's pass is: " + extractedData.Password);
+
+                            }
+
+                        }
+
+        }));
+
+    }
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
